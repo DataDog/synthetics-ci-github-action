237b162c0b82b151c0ae1ed5a46453d98aca00f7
Homere Faivre <homere.faivre@datadoghq.com>
2021-09-30 14:29:52 +0200

    update core github action logic

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,46 +1,66 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import chalk from 'chalk'
 import {expect, test} from '@jest/globals'
-import {run} from '../src/main'
-import {runTests} from '../src/run-tests'
-
-//set timeout long enough for tests to run
-jest.setTimeout(20000)
+import * as runTests from '@datadog/datadog-ci/dist/commands/synthetics/run-test'
+import { config } from '../src/fixtures'
+import * as core from '@actions/core'
+import run from '../src/main'
 
 jest.mock('@actions/core', () => {
-  const originalModule = jest.requireActual('@actions/core');
+  const originalModule = jest.requireActual('@actions/core')
 
   return {
     __esModule: true,
     ...originalModule,
-    getInput: jest.fn((arg) => {
-      switch(arg){   
-        case 'apiKey': return "xxx";
-        case 'appKey': return "yyy";
-        case 'publicIds': return 'public_id1'; 
-    }
+    getInput: jest.fn(arg => {
+      switch (arg) {
+        case 'apiKey':
+          return 'xxx'
+        case 'appKey':
+          return 'yyy'
+        case 'publicIds':
+          return 'public_id1'
+      }
     })
-  };
-});
-
-jest.mock('../src/run-tests')
-jest.mock('@datadog/datadog-ci/dist/commands/synthetics/run-test')
-const context = {stdout: process.stdout, stderr: process.stderr} as any
-
-test('Github Action calls runTests', async () => {
-  await run()
-  expect(runTests).toHaveBeenCalledWith('xxx', 'yyy',['public_id1'], context );
+  }
 })
+
+test('Github Action called with dummy parameter fails ', async () => {
+  const setFailedMock = jest.spyOn(core, 'setFailed')
+  await run()
+  expect(setFailedMock).toHaveBeenCalledWith('Running Datadog Synthetics tests failed.')
+})  
+
+test('runTests called with minimum require params provided by Github Action', async () => {
+  jest
+  .spyOn(runTests, 'executeTests')
+  .mockImplementation(() => ({} as any))
+ 
+  await run()
+  expect(runTests.executeTests).toHaveBeenCalledWith(expect.anything(), {...config , apiKey:'xxx', appKey: 'yyy', publicIds: ['public_id1']})
+}) 
+
 
 test('Github Action runs from js file', async () => {
 
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-
-  const result = await new Promise<string>((resolve, reject) => cp.execFile(np, [ip], (error, stdout, stderr) => error ? reject(error) : resolve(stdout.toString())))
-  expect(result).toContain(`Missing ${chalk.red.bold('DATADOG_API_KEY')} in your environment.`)
-  
+  try{
+    const result = await new Promise<string>((resolve, reject) => 
+      cp.execFile(np, [ip], (error, stdout, stderr) =>
+        error ? reject(error) : resolve(stdout.toString())
+      )
+  )
+  }catch(error){
+    expect(error.code).toBe(1)
+  }
 })
+
+ 
+
+
+
+
+
 
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,143 @@
 import * as core from '@actions/core'
-import { runTests } from './run-tests'
+import chalk from 'chalk'
+import {DefaultReporter} from '@datadog/datadog-ci/dist/commands/synthetics/reporters/default'
+import {executeTests} from '@datadog/datadog-ci/dist/commands/synthetics/run-test'
+import {CiError} from '@datadog/datadog-ci/dist/commands/synthetics/errors'
+import {CommandConfig} from '@datadog/datadog-ci/dist/commands/synthetics/interfaces'
+import {getReporter} from '@datadog/datadog-ci/dist/commands/synthetics/utils'
+import {BaseContext} from 'clipanion'
+import deepExtend from 'deep-extend'
 
+const DEFAULT_CONFIG: CommandConfig = {
+  apiKey: '',
+  appKey: '',
+  configPath: 'datadog-ci.json',
+  datadogSite: 'datadoghq.com',
+  failOnCriticalErrors: false,
+  failOnTimeout: true,
+  files: ['{,!(node_modules)/**/}*.synthetics.json'],
+  global: {},
+  locations: [],
+  pollingTimeout: 2 * 60 * 1000,
+  proxy: {protocol: 'http'},
+  publicIds: [],
+  subdomain: 'app',
+  tunnel: false
+}
 
-export async function run(): Promise<void> {
+const removeUndefinedValues = <T extends {[key: string]: any}>(object: T): T => {
+  const newObject = {...object}
+  Object.keys(newObject).forEach((k) => newObject[k] === undefined && delete newObject[k])
+
+  return newObject
+}
+
+const run = async (): Promise<void> => {
+  const apiKey = core.getInput('apiKey')
+  const appKey = core.getInput('appKey')
+  const publicIds = core.getInput('publicIds')?.split(',')
+  const datadogSite = core.getInput('datadogSite')
+  const configPath = core.getInput('configPath')
+  const files = core.getInput('files')?.split(',')
+  const testSearchQuery = core.getInput('testSearchQuery')
+  const subdomain = core.getInput('subdomain')
+  const tunnel = core.getInput('tunnel') as unknown
+
+  const context = {
+    stdin: process.stdin,
+    stdout: process.stdout,
+    stderr: process.stderr
+  } as BaseContext
+
+  const reporter = getReporter([new DefaultReporter(context as any)])
+
+  // Config from Github Actions > default config
+  const config = deepExtend(
+    DEFAULT_CONFIG,
+    removeUndefinedValues({
+      apiKey: apiKey,
+      appKey: appKey,
+      configPath: configPath,
+      datadogSite: datadogSite,
+      files: files,
+      publicIds: publicIds,
+      subdomain: subdomain,
+      testSearchQuery: testSearchQuery,
+      tunnel: tunnel,
+    })
+  )
+
+  if (typeof config.files === 'string') {
+    reporter!.log('[DEPRECATED] "files" should be an array of string instead of a string.\n')
+    config.files = [config.files]
+  }
 
   try {
-    const apiKey = core.getInput('apiKey');
-    const appKey = core.getInput('appKey'); 
-    const publicIds = core.getInput('publicIds').split(",");
-
-    const context = {stdout: process.stdout, stderr: process.stderr} as any
-
-    await runTests(apiKey,appKey,publicIds, context)
-  
+    await executeTests(reporter, config)
   } catch (error) {
-    
+    if (error instanceof CiError) {
+      switch (error.code) {
+        case 'NO_TESTS_TO_RUN':
+          reporter.log('No test to run.\n')
+          break
+        case 'MISSING_APP_KEY':
+          reporter.error(
+            `Missing ${chalk.red.bold(
+              'DATADOG_APP_KEY'
+            )} in your environment.\n`
+          )
+          break
+        case 'MISSING_API_KEY':
+          reporter.error(
+            `Missing ${chalk.red.bold(
+              'DATADOG_API_KEY'
+            )} in your environment.\n`
+          )
+          break
+        case 'POLL_RESULTS_FAILED':
+          reporter.error(
+            `\n${chalk.bgRed.bold(' ERROR: unable to poll test results ')}\n${
+              error.message
+            }\n\n`
+          )
+          break
+        case 'TUNNEL_START_FAILED':
+          reporter.error(
+            `\n${chalk.bgRed.bold(' ERROR: unable to start tunnel')}\n${
+              error.message
+            }\n\n`
+          )
+          break
+        case 'TRIGGER_TESTS_FAILED':
+          reporter.error(
+            `\n${chalk.bgRed.bold(' ERROR: unable to trigger tests')}\n${
+              error.message
+            }\n\n`
+          )
+          break
+        case 'UNAVAILABLE_TEST_CONF':
+          
+          reporter.error(
+            `\n${chalk.bgRed.bold(
+              ' ERROR: unable to obtain test configurations with search query '
+            )}\n${error.message}\n\n`
+          )
+          break
+        case 'UNAVAILABLE_TUNNEL_CONF':
+          reporter.error(
+            `\n${chalk.bgRed.bold(
+              ' ERROR: unable to get tunnel configuration'
+            )}\n${error.message}\n\n`
+          )
+      }
+    }
+    core.setFailed('Running Datadog Synthetics tests failed.')
   }
 }
 
-if (require.main === module){
+if (require.main === module) {
   run()
 }
+
+export default run
+
