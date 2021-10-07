c9cbab50ec6e95c160bc4401181d8d713f06178c
Homere Faivre <homere.faivre@datadoghq.com>
2021-10-07 17:31:40 +0200

    render executeTest results

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {CiError} from '@datadog/datadog-ci/dist/commands/synthetics/errors'
 import {DefaultReporter} from '@datadog/datadog-ci/dist/commands/synthetics/reporters/default'
 import {executeTests} from '@datadog/datadog-ci/dist/commands/synthetics/run-test'
 import {getReporter} from '@datadog/datadog-ci/dist/commands/synthetics/utils'
-import {Summary} from '@datadog/datadog-ci/dist/commands/synthetics/interfaces'
+import {handleResults, renderResults} from './process-results'
 
 import {BaseContext} from 'clipanion'
 import {resolveConfig} from './resolve-config'
@@ -18,39 +18,22 @@ const run = async (): Promise<void> => {
       stderr: process.stderr
     } as BaseContext
   }
-
+  const startTime = Date.now()
   const reporter = getReporter([new DefaultReporter(context as any)])
   const config = await resolveConfig()
 
   try {
-    const {results, summary, tests, triggers} = await executeTests(
-      reporter,
-      config
-    )
-    if (
-      summary.criticalErrors > 0 ||
-      summary.failed > 0 ||
-      summary.timedOut > 0 ||
-      summary.notFound > 0
-    )
-      core.setFailed(
-        `Datadog Synthetics tests failed :\n${printSummary(summary)}`
-      )
+    const {results, summary, tests, triggers} = await executeTests(reporter, config)
+    const resultSummary = renderResults(results, summary, tests, triggers, config, reporter, startTime)
+    handleResults(resultSummary)
   } catch (error) {
+    console.log(error)
     if (error instanceof CiError) {
       reportCiError(error, reporter)
     }
     core.setFailed('Running Datadog Synthetics tests failed.')
   }
 }
-
-const printSummary = (summary: Summary) =>
-`criticalErrors: ${summary.criticalErrors},
-passed: ${summary.passed},
-failed: ${summary.failed},
-skipped: ${summary.skipped},
-notFound: ${summary.notFound},
-timedOut: ${summary.timedOut}`
 
 if (require.main === module) {
   run()
---
+++ src/process-results.ts
@@ -0,0 +1,115 @@
+import * as core from '@actions/core'
+import { ERRORS, ExecutionRule, LocationsMapping, MainReporter, PollResult, Summary, SyntheticsCIConfig, Test, Trigger } from "@datadog/datadog-ci/dist/commands/synthetics/interfaces"
+import { hasTestSucceeded, isCriticalError } from "@datadog/datadog-ci/dist/commands/synthetics/utils"
+
+export const handleResults = (summary : Summary) => {
+    if (
+      summary.criticalErrors > 0 ||
+      summary.failed > 0 ||
+      summary.timedOut > 0 ||
+      summary.notFound > 0
+    ) 
+    core.setFailed(
+        `Datadog Synthetics tests failed : ${printSummary(summary)}`
+      )
+      
+}
+export const renderResults = (
+    results: {[key: string]: PollResult[]},
+    summary: Summary,
+    tests: Test[],
+    triggers: Trigger,
+    config : SyntheticsCIConfig,
+    reporter: MainReporter,
+    startTime: number
+  ) => {
+    // Sort tests to show success first then non blocking failures and finally blocking failures.
+    tests.sort(sortTestsByOutcome(results, config))
+  
+    // Rendering the results.
+    reporter?.reportStart({startTime})
+    const locationNames = triggers.locations.reduce((mapping, location) => {
+      mapping[location.id] = location.display_name
+  
+      return mapping
+    }, {} as LocationsMapping)
+    let hasSucceeded = true // Determine if all the tests have succeeded
+    for (const test of tests) {
+        
+      const testResults = results[test.public_id]
+      
+      if (!summary.timedOut) {
+        summary.timedOut = 0
+      }
+  
+      const hasTimeout = testResults.some((pollResult) => pollResult.result.error === ERRORS.TIMEOUT)
+      if (hasTimeout) {
+        summary.timedOut++
+      }
+      
+  
+      if (!config.failOnCriticalErrors) {
+        if (!summary.criticalErrors) {
+          summary.criticalErrors = 0
+        }
+        const hasCriticalErrors = testResults.some((pollResult) => isCriticalError(pollResult.result))
+        if (hasCriticalErrors) {
+          summary.criticalErrors++
+        }
+      }
+  
+      const passed = hasTestSucceeded(testResults, config.failOnCriticalErrors, false)
+      if (passed) {
+          console.log('ha')
+        summary.passed++
+      } else {
+        summary.failed++
+        if (test.options.ci?.executionRule !== ExecutionRule.NON_BLOCKING) {
+          hasSucceeded = false
+        }
+      }
+  
+      reporter?.testEnd(
+        test,
+        testResults,
+        `https://${config.subdomain}.${config.datadogSite}/`,
+        locationNames,
+        config.failOnCriticalErrors,
+        false
+      )
+    }
+  
+    reporter?.runEnd(summary)
+  
+    return summary
+  }
+  
+export const sortTestsByOutcome = (results: {[key: string]: PollResult[]}, config: SyntheticsCIConfig) => {
+    return (t1: Test, t2: Test) => {
+      const success1 = hasTestSucceeded(
+        results[t1.public_id],
+        config.failOnCriticalErrors,
+        false
+      )
+      const success2 = hasTestSucceeded(
+        results[t2.public_id],
+        config.failOnCriticalErrors,
+        false
+      )
+      const isNonBlockingTest1 = t1.options.ci?.executionRule === ExecutionRule.NON_BLOCKING
+      const isNonBlockingTest2 = t2.options.ci?.executionRule === ExecutionRule.NON_BLOCKING
+  
+      if (success1 === success2) {
+        if (isNonBlockingTest1 === isNonBlockingTest2) {
+          return 0
+        }
+  
+        return isNonBlockingTest1 ? -1 : 1
+      }
+  
+      return success1 ? -1 : 1
+    }
+  }
+  
+export const printSummary = (summary: Summary) =>
+`criticalErrors: ${summary.criticalErrors}, passed: ${summary.passed}, failed: ${summary.failed}, skipped: ${summary.skipped}, notFound: ${summary.notFound}, timedOut: ${summary.timedOut}`
