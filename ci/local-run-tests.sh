cb6efda100e560a2dd10f3bcf35c0b20d40f7f98
Etienne Lefebvre <etienne.lefebvre@datadoghq.com>
2022-03-24 17:36:31 +0100

    [dev] add local-run-test script w/ context

--- ci/local-run-tests.sh
+++ ci/local-run-tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Run the Github Action though build dist/ files for developement with default parameters,
+# inputs provided by the Github Action config are given through `INPUT_` environement variables,
+# comma-separated public IDs to run should be provided as the first argument.
+
 if [ $# -eq 0 ]; then
     echo "usage: local-run-test.sh pub-lic-ids[,abc-def-hij]"
     exit 1
