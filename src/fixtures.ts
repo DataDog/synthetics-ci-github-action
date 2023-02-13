ea9a637d4be4fc9d31212639baf5f86ebcf84618
corentin <corentin.girard@datadoghq.com>
2023-02-13 10:43:58 +0100

    Add `junit_report` input

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -31,3 +31,14 @@ export const mockReporter: synthetics.MainReporter = {
   testWait: jest.fn(),
   testsWait: jest.fn(),
 }
+
+export const EMPTY_SUMMARY: synthetics.Summary = {
+  criticalErrors: 0,
+  passed: 0,
+  failed: 0,
+  failedNonBlocking: 0,
+  skipped: 0,
+  testsNotFound: new Set(),
+  timedOut: 0,
+  batchId: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+}
