d638659dca7a9db0d7c2482790563042e5356f12
corentin <corentin.girard@datadoghq.com>
2025-04-25 13:02:25 +0200

    Remove redundant newlines

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ const run = async (): Promise<void> => {
     if (exitReason !== 'passed') {
       core.setFailed(`Datadog Synthetics tests failed: ${getTextSummary(summary, batchUrl)}`)
     } else {
-      core.info(`\n\nDatadog Synthetics tests succeeded: ${getTextSummary(summary, batchUrl)}`)
+      core.info(`Datadog Synthetics tests succeeded: ${getTextSummary(summary, batchUrl)}`)
     }
   } catch (error) {
     synthetics.utils.reportExitLogs(reporter, config, {error})
