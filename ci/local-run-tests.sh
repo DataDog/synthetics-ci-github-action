87730d607a291acd16ac6a9391b512ad4684c8b0
Corentin Girard <corentin.girard@datadoghq.com>
2025-04-25 13:24:15 +0200

    [SYNTH-10268] Improve e2e tests (#308)
    * Run e2e tests at every commit
    
    * Rename e2e tests file

--- ci/local-run-tests.sh
+++ ci/local-run-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Run the Github Action through build dist/ files for developement with default parameters,
+# Run the Github Action through build dist/ files for development with default parameters,
 # inputs provided by the Github Action config are given through `INPUT_` environment variables,
 # comma-separated public IDs to run should be provided as the first argument.
 
 if [ $# -eq 0 ]; then
-    echo "usage: local-run-test.sh pub-lic-ids[,abc-def-hij]"
-    exit 1
+  echo "usage: local-run-tests.sh pub-lic-ids[,abc-def-hij]"
+  exit 1
 fi
 
 set -e
